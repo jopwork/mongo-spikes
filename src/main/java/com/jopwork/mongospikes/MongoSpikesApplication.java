f11d990a2713a5947b12ef924c86a8f0cf6d62b0
jopwork <jopwork@gmail.com>
2018-01-27 01:02:01 +0800

    Created mongo-spikes

---
+++ src/main/java/com/jopwork/mongospikes/MongoSpikesApplication.java
@@ -0,0 +1,12 @@
+package com.jopwork.mongospikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoSpikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoSpikesApplication.class, args);
+	}
+}
